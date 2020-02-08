643abbae7d911581b59ae84909f7b584ba8ac72a
Daniele <danijovina@gmail.com>
2020-02-08 14:01:14 -0300

    Apply gradient background color

--- src/view/index.js
+++ src/view/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  background: linear-gradient(0deg, #168b7d, #168b7d);
+`;
 
 const Main = () => (
-  <div>
+  <Container>
     <p>Chat App</p>
-  </div>
+  </Container>
 );
 
 export default Main;
