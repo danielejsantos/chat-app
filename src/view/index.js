1993d53fd075b38bfe439024a6bda1c7a3fe85a6
Daniele <danijovina@gmail.com>
2020-02-12 20:38:17 -0300

    Add chat and contact details to the main index

--- src/view/index.js
+++ src/view/index.js
@@ -3,18 +3,25 @@ import styled from "styled-components";
 
 import Contacts from "./contacts";
 import SocialMedia from "./social-media";
+import Chat from "./chat";
+import ContactDetails from "./contact-details";
 
 const Container = styled.div`
   display: flex;
   height: 100vh;
+  padding-top: 15px;
   background: linear-gradient(0deg, #168b7d, #168b7d);
   flex-direction: row;
+  box-sizing: border-box;
+  overflow: hidden;
 `;
 
 const Main = () => (
   <Container>
     <Contacts />
     <SocialMedia />
+    <Chat />
+    <ContactDetails />
   </Container>
 );
 
